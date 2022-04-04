b891cc4cf9c280d5dcc5b61edc3d08727a78f00f
trollmannen <97115586+trollmannen@users.noreply.github.com>
2022-04-04 20:06:30 +0200

    Add files via upload

--- Kick-Ya-Chop-AI/main.py
+++ Kick-Ya-Chop-AI/main.py
@@ -3,7 +3,6 @@ import numpy as np
 from mss import mss
 import keyboard as kb
 import pyautogui as pg
-from time import sleep
 
 
 pg.PAUSE = 0
@@ -19,18 +18,18 @@ y = 900
 sct = mss()
 
 dimensions_left = {
-  'left': 254,
-  'top': 650,
-  'width': 183,
-  'height': 160
-  }
+    'left': 254,
+    'top': 650,
+    'width': 183,
+    'height': 160
+}
 
 dimensions_right = {
-  'left': 549,
-  'top': 650,
-  'width': 183,
-  'height': 160
-  }
+    'left': 549,
+    'top': 650,
+    'width': 183,
+    'height': 160
+}
 
 wood_left = cv.imread('wood-left.jpg')
 wood_right = cv.imread('wood-right.jpg')
@@ -39,31 +38,32 @@ w = wood_left.shape[1]
 h = wood_left.shape[0]
 
 while True:
-  if left:
-    scr = np.array(sct.grab(dimensions_left))
-    wood = wood_left
-  else:
-    scr = np.array(sct.grab(dimensions_right))
-    wood = wood_right
-
-  scr_no_alpha = scr[:,:,:3]
-
-  result = cv.matchTemplate(scr_no_alpha, wood, cv.TM_CCOEFF_NORMED)
-
-  _, max_val, _, max_loc = cv.minMaxLoc(result)
-
-  if max_val > .85:
-    left = not left
     if left:
-      x=340
+        scr = np.array(sct.grab(dimensions_left))
+        wood = wood_left
     else:
-      x=600
+        scr = np.array(sct.grab(dimensions_right))
+        wood = wood_right
 
-    cv.rectangle(scr, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255), 2)
+    scr_no_alpha = scr[:, :, :3]
 
-  cv.imshow('screenshot', scr)
-  cv.setWindowProperty('screenshot', cv.WND_PROP_TOPMOST, 1)
-  cv.waitKey(1)
-  pg.click(x=x, y=y)
-  if kb.is_pressed('q'):
-    break
+    result = cv.matchTemplate(scr_no_alpha, wood, cv.TM_CCOEFF_NORMED)
+
+    _, max_val, _, max_loc = cv.minMaxLoc(result)
+
+    if max_val > .85:
+        left = not left
+        if left:
+            x = 340
+        else:
+            x = 600
+
+        cv.rectangle(scr, max_loc, (max_loc[0] + w, max_loc[1] + h),
+                     (0, 255, 255), 2)
+
+    cv.imshow('screenshot', scr)
+    cv.setWindowProperty('screenshot', cv.WND_PROP_TOPMOST, 1)
+    cv.waitKey(1)
+    pg.click(x=x, y=y)
+    if kb.is_pressed('q'):
+        break
